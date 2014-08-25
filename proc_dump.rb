d39778330ccd6e98fe90b35d1619b4735aa5d205
Jeremiah Sheehy <fuzzy@fuzzy-l.(none)>
2014-08-25 03:56:14 -0400

    added /proc search

--- proc_dump.rb
+++ proc_dump.rb
@@ -1,26 +1,51 @@
 #!/usr/bin/ruby
 
 class Proccess
-	attr_reader :number, :name	##	Needed for accessing class vars (Reading/Writing)
-															##	Think created as private
-															## 	For read only/write only attr_reader/attr_writer
+  ##	Needed for accessing class vars (Reading/Writing)
+	attr_accessor :number, :name, :command	
 
-	def initialize (number, name)	
+	##	Think created as private
+ 	## 	For read only/write only attr_reader/attr_writer
+
+	def initialize (number)	
 		@number=number
-		@name=name
 	end
-	
+  
 	def path										##	Example of a "virtual instance variable"
 		"/proc/#{@number}"				##	Think database derived value
 	end	
 	
 	def to_s
-		"proc #{@number} is #{@name}"
+		"#{@number} \t #{@name} \t"
 	end
 end
 
+## Define procs array
+procs=[]
 
-p1 = Proccess.new(10,"newp")
-puts p1
-puts "#{p1.number}:#{p1.name}"
-puts "#{p1.path}"
+## For each file in /proc folder filter for pid's
+Dir.foreach("/proc/") do |file|   
+  if /\d+/.match("#{file}")   ##  Regexp for 1 or more digits
+    proc = Proccess.new(file)
+    procs.push proc
+  end
+end
+
+p procs
+
+procs.each do |p|
+  comm = File.open("#{p.path}/comm", "r")
+  comm.each_line do |line|      ##  File should only contain one line
+    p.name = line
+  end
+  comm.close
+
+  cmdline = File.open("#{p.path}/cmdline", "r")
+  cmdline.each_line do |line|   ##  File should only contain one line
+    p.command = line
+  end
+  cmdline.close
+  puts p
+end
+
+
